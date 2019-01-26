36b05066bf2486823d294a16c67020364d0ffc52
Veradux <veradux@abv.bg>
2019-01-27 00:26:33 +0200

    added a big TODO list in Main

--- src/bg/dnevnik/Comment.java
+++ src/bg/dnevnik/Comment.java
@@ -4,18 +4,26 @@ package bg.dnevnik;
 import bg.dnevnik.exceptions.WrongInputException;
 
 public class Comment extends Post {
+	
+	enum Mood {
+		// Apparently this exists in the website..
+		NEUTRAL, CHEERFUL, CURIOUS, SAD, ANGRY
+	}
+	
+	private final Mood mood;
 
-	private Comment(User poster, String content) throws WrongInputException {
+	private Comment(User poster, String content, Mood mood) throws WrongInputException {
 		super(poster, content);
+		this.mood = mood;
 	}
 	
 	
-	public static void postTo(Article article, User poster, String content) {
+	public static void postTo(Article article, User poster, String content, Mood mood) {
 		// Added wrapper to the comment constructor to divide responsibilities between creating and adding to the article
 		// Also it deals with exception handling in one place, instead of having to try/catch every time a comment is added
 		Comment comment = null;
 		try {
-			comment = new Comment(poster, content);
+			comment = new Comment(poster, content, mood);
 		} catch (WrongInputException e) {
 			e.printStackTrace();
 		}
---
+++ src/bg/dnevnik/Main.java
@@ -0,0 +1,45 @@
+package bg.dnevnik;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/* These are some of the commands we could add, 
+		they should be very helpful as a TODO list.
+		It will also be a part of what the simulated users will do.
+		They are all according to the website in terms of functionality
+		
+		The logic for the command management should probably be in the site class,
+		it'll likely be here just temporarily
+		
+		commands:
+		"help" shows this info about all commands
+		
+		"sign up" requires name, email, and password to create a user
+		"sign in" requires email and password, allows user to make posts and vote
+		"sign out" requires to be signed in, disables ability to make posts and vote
+		
+		"upvote"/"downvote" requires a post and votes on it
+		"comment" requires article and content
+		"write article" requires title, content, category, and keywords to create an article
+		
+		"show all categories" shows all created categories
+		"show hot categories" shows the five categories with most articles
+		"(category name)" shows all articles in this category 
+		(this command should have lower priority over the others, 
+		in case a category has the same name as one of the commands)
+		"show article" requires an article to show it
+		"show comments" requires the last command to have been "show article", 
+		and shows them ordered by date, from old to new
+		
+		These don't require anything, because they use all articles
+		"show from today" 
+		"sort by new"
+		"sort by views"
+		"sort by comments"
+		"sort by votes"
+		
+		*/
+	}
+
+}
