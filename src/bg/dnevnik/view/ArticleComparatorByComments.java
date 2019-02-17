ff0d343ad5cc96a88f16aef722cc8446ad1b9360
I3eco <veselin.g.v@gmail.com>
2019-02-17 22:13:21 +0200

    generated some articles from different dates

--- src/bg/dnevnik/view/ArticleComparatorByComments.java
+++ src/bg/dnevnik/view/ArticleComparatorByComments.java
@@ -8,6 +8,9 @@ public class ArticleComparatorByComments implements Comparator<Article> {
 
 	@Override
 	public int compare(Article o1, Article o2) {
+		if(o1.getCommentsCount() - o2.getCommentsCount() == 0) {
+			return o1.getID() - o2.getID();
+		}
 		return o1.getCommentsCount() - o2.getCommentsCount();
 	}
 
--- src/bg/dnevnik/view/ArticleComparatorByViews.java
+++ src/bg/dnevnik/view/ArticleComparatorByViews.java
@@ -8,6 +8,9 @@ public class ArticleComparatorByViews implements Comparator<Article> {
 
 	@Override
 	public int compare(Article o1, Article o2) {
+		if(o1.getNumberOfViews() - o2.getNumberOfViews() == 0) {
+			return o1.getID() - o2.getID();
+		}
 		return o1.getNumberOfViews() - o2.getNumberOfViews();
 	}
 
